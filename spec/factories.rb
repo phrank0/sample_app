02986afedf1512c1b5a2affdd14f7d115d80ad82
Frank J. Gualtieri <phrank.0@gmail.com>
2013-04-03 02:31:25 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Frank J. Gualtieri"
+    email    "phrank.0@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
