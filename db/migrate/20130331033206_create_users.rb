7412d7129741161f8105d3f3c1a02fca17deb33e
Frank J. Gualtieri <phrank.0@gmail.com>
2013-04-21 23:23:25 -0400

    fix migrations

--- db/migrate/20130331033206_create_users.rb
+++ db/migrate/20130331033206_create_users.rb
@@ -1,11 +1,5 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :microposts do |t|
-      t.string :content
-      t.integer :user_id
-
-      t.timestamps
-    end
     create_table :users do |t|
       t.string :name
       t.string :email
