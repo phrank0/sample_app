f5a375d1ef989e2451ccd41fca9cc698bc8bf8fc
Frank J. Gualtieri <phrank.0@gmail.com>
2013-04-21 22:02:54 -0400

    fix migrations

--- db/migrate/20130331033206_create_users.rb
+++ db/migrate/20130331033206_create_users.rb
@@ -1,5 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :microposts do |t|
+      t.string :content
+      t.integer :user_id
+
+      t.timestamps
+    end
     create_table :users do |t|
       t.string :name
       t.string :email
--- db/migrate/20130412015116_create_microposts.rb
+++ db/migrate/20130412015116_create_microposts.rb
@@ -1,10 +1,12 @@
 class CreateMicroposts < ActiveRecord::Migration
   def change
+=begin
     create_table :microposts do |t|
       t.string :content
       t.integer :user_id
 
       t.timestamps
     end
+=end
   end
 end
