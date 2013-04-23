d2f1a84b5a623cc443daa10d0cee25b87b7456b8
Frank J. Gualtieri <phrank.0@gmail.com>
2013-04-23 00:53:56 -0400

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,6 +35,17 @@ describe "Static pages" do
       specify "sidebar should contain properly-pluralized micropost count" do
         should have_selector('aside', text: '2 microposts')
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
   end
 
